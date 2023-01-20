e23cc4e9aed460923a5c5341f643c54c81acf338
Chloe <chloevision97@gmail.com>
2023-01-20 05:25:31 +0100

    Added Context to Home Page

--- src/component/home/home.js
+++ src/component/home/home.js
@@ -5,10 +5,22 @@ function Home(props) {
     <main>
       <section className="info">
         <h2>Our Mission</h2>
-        <p>
-          Bring Stuffed Animals to all boys and girls {props.items[0].addToCart}{" "}
-          asdkasdjaskldjaskldjaskdjqkwjheiasjdaskdmadadadjadasjdkasdjoqiehjqlkdjhqwoiehaslkdjasldhqwdjalskdjhaslkdjhasliehqwldhasldhad
-        </p>
+        <div className="context">
+          <p>
+            Bring Stuffed Animals to all boys and girls woman and men and
+            nonbinary folks no matter your identify or how you identify our goal
+            is bring a smile to everyone with our affordable prices
+          </p>
+          <p>You can buy it here Online or in our Local store In Munich also</p>
+        </div>
+        <h2>Bring a smile</h2>
+        <div className="context">
+          <p>
+            We have Project for each Stuffed Animal Sold in our Online shop we
+            donate 1$ to the TransYouth Foundation and every 10 animals sold we
+            also donate a stuffed animal to the same assosiation{" "}
+          </p>
+        </div>
       </section>
     </main>
   );
